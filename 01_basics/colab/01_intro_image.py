8318360d8ca13b03d8e692138d19e44feba1faa7
Sylwia1990 <s.salacinska@gmail.com>
2020-03-25 01:09:53 +0100

    add basics

---
+++ 01_basics/colab/01_intro_image.py
@@ -0,0 +1,7 @@
+import cv2
+
+print(cv2.__version__)
+
+image = cv2.imread(filename=r'C:\Users\ssalacinska\PycharmProjects\computer-vision-course\01_basics\assets\bear.jpg')
+cv2.imshow(winname='image', mat=image)
+cv2.waitKey(delay=0)
